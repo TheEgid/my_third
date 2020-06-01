1072d42b40422fef372cd627a5d2c029ef387ce7
TheEgid <egid.f1@gmail.com>
2020-06-01 20:55:18 +0300

    Updates

--- app/functional_tests/conftest.py
+++ app/functional_tests/conftest.py
@@ -1,26 +1,27 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from django.conf import settings
 
 
 @pytest.fixture(scope="class")
 def setup_browser(request):
+    options = Options()
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless=True")
-    chrome_options.add_argument("--log-level=3")
+    options.headless = True
+
     if settings.OPERATING_SYSTEM == "Windows":
-        browser = webdriver.Chrome(options=chrome_options)
+        browser = webdriver.Firefox(options=options)
     else:  # Linux
         selenium_hub_url = 'http://hub:4444/wd/hub'
         browser = webdriver.Remote(
-                command_executor=selenium_hub_url,
-                desired_capabilities=DesiredCapabilities.CHROME,
-                options=chrome_options)
+            command_executor=selenium_hub_url,
+            desired_capabilities=DesiredCapabilities.FIREFOX,
+            options=options)
 
     request.cls.browser = browser
-    browser.implicitly_wait(2)
+    browser.implicitly_wait(time_to_wait=settings.SELENIUM_WAIT)
 
     yield browser
 
